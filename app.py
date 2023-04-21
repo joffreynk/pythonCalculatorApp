c36c06954a7a70f28c119dffc9509de098cf6373
Joffreynk <joyjoffrey@gmail.com>
2023-04-21 17:18:22 +0200

    add logic and lvalidations

--- app.py
+++ app.py
@@ -18,3 +18,45 @@ def operations():
   4. Divide
   5. Close The App
   ''')
+
+while True:
+  operations()
+  operation = input("Enter operation: ")
+  if operation == '5':
+    print("""
+Thank you for using our App
+Bye
+      """)
+    break
+
+  if operation not in ('1', '2', '3', '4', '5'):
+    print("OOOOps, your operation is not valid, please Select 1, 2, 3, 4, and 5")
+    continue
+  while True:
+    try:
+      num1 = float(input("Enter the first number: "))
+      break
+    except ValueError:
+      print("ooops,  please enter a valid number")
+      continue
+
+  while True:
+    try:
+      num2 = float(input("Enter the second value: "))
+      break
+    except ValueError:
+      print("ooops, please enter a valid number")
+      continue
+
+  match operation:
+    case '1':
+      print(num1, " + ", num2, " = ", addition(num1, num2))
+    case '2':
+      print(num1, " - ", num2, " = ", substraction(num1, num2))
+    case '3':
+      print(num1, " * ", num2, " = ", multiplication(num1, num2))
+    case '4':
+      print(num1, " / ", num2, " = ", division(num1, num2))
+    case _:
+      print("ooops, you have selected wrong operation, please Redo")
+      continue
