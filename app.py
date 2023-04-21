a36919a5e43536bbc6a59e56288d8c62305b2aa5
Joffreynk <joyjoffrey@gmail.com>
2023-04-21 17:17:38 +0200

    add operations

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ def multiplication(num1, num2):
 
 def substraction(num1, num2):
   return  round(num1 - num2, 2)
+
+def operations():
+  print('''
+  Select operation:
+  1. Add.
+  2.Sustract.
+  3. Multiply.
+  4. Divide
+  5. Close The App
+  ''')
