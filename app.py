ba2b0756d1ba4326297404ab93d533151aa47e82
Joffreynk <joyjoffrey@gmail.com>
2023-04-21 17:16:55 +0200

    create functions

---
+++ app.py
@@ -0,0 +1,10 @@
+def addition(num1, num2):
+  return  round(num1 + num2, 2)
+def division(num1, num2):
+  return round(num1 / num2, 2)
+
+def multiplication(num1, num2):
+  return  round(num1 * num2, 2)
+
+def substraction(num1, num2):
+  return  round(num1 - num2, 2)
